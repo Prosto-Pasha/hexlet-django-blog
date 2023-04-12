c30ab4460bada14a5692eb974139a9e0bdd8b3d2
Pavel Belyavskiy <prostopasha@gmail.com>
2023-04-12 19:13:15 +0300

    Refactor views

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from hexlet_django_blog.article import views
-
+from hexlet_django_blog.article.views import HomePageView
 
 urlpatterns = [
-    path('', views.index),
+    path('', HomePageView.as_view(template_name='index.html')),
+    # path('', views.index),
 ]
