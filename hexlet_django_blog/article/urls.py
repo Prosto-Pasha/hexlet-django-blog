69a19d9a82df5cd7c1ccf6d91994cc8aa69b6f1e
Pavel Belyavskiy <prostopasha@gmail.com>
2023-04-13 15:44:58 +0300

    Refactor article views.py, urls.py
    Add reverse_lazy, dynamic path

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,6 +3,11 @@ from hexlet_django_blog.article import views
 from hexlet_django_blog.article.views import HomePageView
 
 urlpatterns = [
-    path('', HomePageView.as_view(template_name='index.html')),
-    # path('', views.index),
+    # path('', views.index),  # (1)
+    path('', HomePageView.as_view(template_name='index.html')),  # (2)
+    path(
+        '<str:tags>/<int:article_id>/',
+        HomePageView.as_view(template_name='index.html'),
+        name='article',
+    ),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,20 +1,11 @@
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 
-class HomePageView(TemplateView):
-
-    template_name = "index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['who'] = 'Hexlet'
-        context['author'] = 'Internet'
-        return context
-
-
-# def index(request):
+# def index(request):  # (1)
 #    # return HttpResponse('article')
 #    return render(
 #        request,
@@ -24,3 +15,25 @@ class HomePageView(TemplateView):
 #            'author': 'Internet'
 #        }
 #    )
+
+
+class HomePageView(TemplateView):  # (2)
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['who'] = 'Hexlet'
+        context['author'] = 'Internet'
+        return context
+
+    def get(self, request, *args, **kwargs):
+        tags=self.kwargs.get('tags', '---')
+        article_id=self.kwargs.get('article_id', '---')
+        if tags == '---' and article_id == '---':
+            return redirect(reverse_lazy('article', kwargs={'tags': 'python', 'article_id': 42}))
+        return HttpResponse(f'Статья номер {article_id}. Тег {tags}')
+
+
+# def index(request, tags='python', article_id='42'):
+#    return redirect(f'/article/{tags}/{article_id}')
