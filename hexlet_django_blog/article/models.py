1766e34f1d035d63229f8c322f2c0e198981cbb1
Pavel Belyavskiy <prostopasha@gmail.com>
2023-04-15 10:39:22 +0300

    Refactor comment

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Article(models.Model):
-    name = models.CharField(max_length=200)  # название статьи
-    body = models.TextField()  # тело статьи
+    name = models.CharField(max_length=200)  # Название статьи
+    body = models.TextField()  # Тело статьи
     timestamp = models.DateTimeField(auto_now_add=True)
