392bf309c8df6af248decf3012bd44ab08a586fd
Pavel Belyavskiy <prostopasha@gmail.com>
2023-04-17 13:42:00 +0300

    Refactor models.py object name

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -4,3 +4,6 @@ class Article(models.Model):
     name = models.CharField(max_length=200)  # Название статьи
     body = models.TextField()  # Тело статьи
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.id}) {self.name}'
