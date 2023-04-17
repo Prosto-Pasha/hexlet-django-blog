dcb03410aafba465fb3fa8b51918da2e2a48054b
Pavel Belyavskiy <prostopasha@gmail.com>
2023-04-17 13:55:25 +0300

    Refactor admin.py modify model view

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,8 +1,37 @@
 from django.contrib import admin
+from django.contrib.admin import DateFieldListFilter
+
 from .models import Article
 
 
-class ArticleAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'body']
+######
+# 1
+# admin.site.register(Article)
+######
 
-admin.site.register(Article, ArticleAdmin)
+
+######
+# 2
+# class ArticleAdmin(admin.ModelAdmin):
+#    search_fields = ['name', 'body']
+#
+# admin.site.register(Article, ArticleAdmin)
+######
+
+
+######
+# 3
+# @admin.register(Article)
+# class ArticleAdmin(admin.ModelAdmin):
+#    search_fields = ['name', 'body']
+######
+
+
+######
+# 4
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'name', 'timestamp') # Перечисляем поля, отображаемые в таблице списка статей
+    search_fields = ['name', 'body']
+    list_filter = (('timestamp', DateFieldListFilter),) # Перечисляем поля для фильтрации
+######
