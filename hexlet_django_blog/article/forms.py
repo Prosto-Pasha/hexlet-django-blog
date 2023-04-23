1741305ef7113f200be305f5c8ac86a1744b38b9
Pavel Belyavskiy <prostopasha@gmail.com>
2023-04-24 00:35:59 +0300

    Refactor templates

--- hexlet_django_blog/article/forms.py
+++ hexlet_django_blog/article/forms.py
@@ -1,8 +1,21 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Article
 
 
-class ArticleForm(ModelForm):
+class ArticleForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Название статьи'
+        }
+    ))
+    body = forms.CharField(widget=forms.Textarea(
+        attrs={
+            'class': 'form-control',
+            'placeholder': 'Тело статьи'
+        }
+    ))
+
     class Meta:
         model = Article
         fields = ['name', 'body']
